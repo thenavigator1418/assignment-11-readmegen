59a29aae8c1ea088e216158bdc8f2c2863c4a959
thenavigator1418 <henrydavis95@hotmail.co.uk>
2023-12-19 06:14:04 +0000

    Questions written

--- index.js
+++ index.js
@@ -4,9 +4,50 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = () => {
+    return inquirer.createPromptModule([
+    {
+        type: 'input',
+      name: 'projName',
+      message: 'What is the name of your project?',
+    },
+    {
+      type: 'input',
+      name: 'projDesc',
+      message: 'Provide a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'Provide installation instructions for your project',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions on how to use your project',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Select the type of project license you wish to use',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Provide details of any contributions and credits for your project.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide details of any test conditions used to develop this project.',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Provide your Github username so users can contact you if they have additional questions',
+      },
+    ]);
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
